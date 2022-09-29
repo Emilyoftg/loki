57b9a776afc6a33826009447822d016413896b1c
Emily <71280699+Emilyoftg@users.noreply.github.com>
2022-09-29 06:37:49 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/meenakshikutty/loki.git /loki
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /loki
+fi
+cd /loki
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
