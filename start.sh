24a3d235f0b690ecbc3cebae007cc640528fb331
Emily <71280699+Emilyoftg@users.noreply.github.com>
2022-09-29 06:44:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/meenakshikutty/loki.git /loki
+  git clone https://github.com/Emilyoftg/loki.git /loki
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /loki
